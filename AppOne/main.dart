eaadc70b635ba611e108df44ff5ca1be70654eb8
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-07-20 22:30:45 +0530

    Add files via upload

--- AppOne/main.dart
+++ AppOne/main.dart
@@ -10,7 +10,8 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  List names = ['a','b','c','d'];
+  List names = ['a', 'b', 'c', 'd'];
+
   /*scrollDirection: Axis.horizontal,
             children: [
               Container(
@@ -29,27 +30,46 @@ class MyApp extends StatelessWidget {
                 width: 500,
                 color: Colors.lightGreen[200]),
             ],*/
+  void ontap() {
+    print("hoyeee");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.blueGrey[600],
-          appBar: AppBar(
-            title: Text("Android"),
-            backgroundColor: Colors.lightGreen[300],
-            elevation: 0,
-            // same z index
-            leading: Icon(Icons.menu),
-            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.adb))],
-            centerTitle: true,
-          ),
-          body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) => ListTile(
-              title: Text(names[index%4]+index.toString()),
+        backgroundColor: Colors.blueGrey[600],
+        appBar: AppBar(
+          title: Text("Android"),
+          backgroundColor: Colors.lightGreen[300],
+          elevation: 0,
+          // same z index
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.adb))],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: GestureDetector(
+            onDoubleTap: () {
+              //func
+              ontap();
+            },
+            child: Container(
+              height: 200,
+              width: 100,
+              color: Colors.lightGreen[300],
+              child: const Center(
+                child: Text(
+                  "Tap MEH",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ),
+        ),
       ),
     );
   }
