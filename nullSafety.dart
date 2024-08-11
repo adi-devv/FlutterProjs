f6c14df6ff11f97257fc51ea7811692c884642a3
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-08-12 00:18:37 +0530

    Add files via upload

--- nullSafety.dart
+++ nullSafety.dart
@@ -16,8 +16,41 @@ String nameInApp = nameFromDatabase ?? 'No name';
 
 - if not null -> returns normally
 - if null -> returns null
+
+?. gracefully handles null values without your app crashing
+
+Conditional check was used before NULL SAFETY:
+
+if(a!=null){}
 */
+
+class Student {
+  final String name;
+  final int? score;
+
+  Student({required this.name, this.score});
+}
+
+String scoreAsPerc(int? score) {
+  int total = 100;
+  double perc = (score?.toDouble() ?? 0) * 100 / total;
+
+  return score==null?"Absent":"${perc.toStringAsFixed(2)}%";
+}
 
 void main() {
   print(nameFromDatabase!.length);
+
+  List<Student> s = [
+    Student(name: "Aa", score: 59),
+    Student(name: "Za"),
+    Student(name: "Ba", score: 69),
+    Student(name: "Ca", score: 79),
+    Student(name: "Da"),
+  ];
+  for (var stud in s){
+    print("${stud.name}'s percentage: ${scoreAsPerc(stud.score)}");
+
+  }
+
 }
