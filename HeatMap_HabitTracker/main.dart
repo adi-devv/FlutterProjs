477e58576b4ad694dd6f0db9e5109f44183798ff
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-08-15 16:04:18 +0530

    Add files via upload

--- HeatMap_HabitTracker/main.dart
+++ HeatMap_HabitTracker/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:heatmap_habit/database/habit_database.dart';
 import 'package:heatmap_habit/pages/home_page.dart';
-import 'package:heatmap_habit/theme/dark_mode.dart';
-import 'package:heatmap_habit/theme/light_mode.dart';
 import 'package:heatmap_habit/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HabitDatabase.initialize();
+  await HabitDatabase().saveFirstLaunchDate();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => HabitDatabase(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
