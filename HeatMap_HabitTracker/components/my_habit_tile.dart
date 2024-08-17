424b75d54d7984c3a95b546bfde917e6bf65492f
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-08-17 15:12:39 +0530

    Add files via upload

--- HeatMap_HabitTracker/components/my_habit_tile.dart
+++ HeatMap_HabitTracker/components/my_habit_tile.dart
@@ -19,49 +19,53 @@ class MyHabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const StretchMotion(),
-        children: [
-          SlidableAction(
-            onPressed: editHabit,
-            backgroundColor: Colors.grey.shade800,
-            icon: Icons.edit,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          SlidableAction(
-            onPressed: deleteHabit,
-            backgroundColor: Colors.red,
-            icon: Icons.delete,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ],
-      ),
-      child: GestureDetector(
-        onTap: () {
-          if (onChanged != null) {
-            onChanged!(!isCompleted);
-          }
-        },
-        child: Container(
-          decoration: BoxDecoration(
-              color: isCompleted
-                  ? Colors.green.shade300
-                  : Theme.of(context).colorScheme.secondary,
-              borderRadius: BorderRadius.circular(8)),
-          padding: const EdgeInsets.all(8),
-          margin: const EdgeInsets.symmetric(vertical: 5),
-          child: ListTile(
-            title: Text(
-              text,
-              style: TextStyle(
-                decoration: isCompleted ? TextDecoration.lineThrough : null,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: editHabit,
+              backgroundColor: Colors.grey.shade800,
+              icon: Icons.edit,
+              borderRadius: BorderRadius.circular(10),
             ),
-            leading: Checkbox(
-              activeColor: Colors.green,
-              value: isCompleted,
-              onChanged: onChanged,
+            SlidableAction(
+              onPressed: deleteHabit,
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ],
+        ),
+        child: GestureDetector(
+          onTap: () {
+            if (onChanged != null) {
+              onChanged!(!isCompleted);
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: isCompleted
+                    ? Colors.green.shade300
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(8)),
+            padding: const EdgeInsets.all(12),
+            child: ListTile(
+              title: Text(
+                text,
+                style: TextStyle(
+                    decoration: isCompleted ? TextDecoration.lineThrough : null,
+                    color: Colors.white,
+                    decorationColor: Colors.green,
+                    decorationThickness: 2),
+              ),
+              leading: Checkbox(
+                activeColor: Colors.green,
+                value: isCompleted,
+                onChanged: onChanged,
+              ),
             ),
           ),
         ),
---
+++ HeatMap_HabitTracker/components/my_heatmap.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHeatmap extends StatelessWidget {
+  const MyHeatmap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
