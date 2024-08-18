452fe7868be97b28eb53b2fdbf475bf1c342e8a6
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-08-18 21:38:12 +0530

    Add files via upload

---
+++ UserAuth/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          Icon(Icons.lock),
+        ],
+      ),
+    );
+  }
+}
