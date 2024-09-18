5a2940f265d9130d4dc0f0e72f3c714dfcf7ab9e
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-09-18 11:48:12 +0530

    Add files via upload

--- ChatApp/pages/home_page.dart
+++ ChatApp/pages/home_page.dart
@@ -1,27 +1,17 @@
-import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout(){
-    final _auth = AuthService();
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: Icon(Icons.logout),
-          ),
-        ],
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
